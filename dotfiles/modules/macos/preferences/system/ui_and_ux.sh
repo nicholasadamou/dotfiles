1c453a2dcfd49aaac6541854730ebe0102e0d790
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-05-21 05:50:59 -0400

    chore: Update wallpaper and system preferences in macOS

--- dotfiles/modules/macos/preferences/system/ui_and_ux.sh
+++ dotfiles/modules/macos/preferences/system/ui_and_ux.sh
@@ -5,7 +5,7 @@ declare -r DESKTOP_WALLPAPER_PATH="${PWD}/system/wallpapers/magma.jpg"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 if brew info wallpaper &>/dev/null; then
-	wallpaper set "$DESKTOP_WALLPAPER_PATH"
+    wallpaper set "$DESKTOP_WALLPAPER_PATH"
 fi
 
 # Customize System Preferences and Behaviors in macOS:
@@ -36,11 +36,11 @@ defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
 # Change the Mac's Host Name
 model=$(system_profiler SPHardwareDataType | grep "Model Name" | awk -F": " '{print $2}') # Get Mac model information
-HOST_NAME="Nicholas's $model" # E.g., 'Nicholas's MacBook Pro' or 'Nicholas's iMac'
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string `${HOST_NAME}` && \
-		sudo scutil --set ComputerName ${HOST_NAME} && \
-		sudo scutil --set HostName ${HOST_NAME}  && \
-		sudo scutil --set LocalHostName ${HOST_NAME}
+HOST_NAME="Nicholas's $model"                                                             # E.g., 'Nicholas's MacBook Pro' or 'Nicholas's iMac'
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "${HOST_NAME}" &&
+    sudo scutil --set ComputerName "${HOST_NAME}" &&
+    sudo scutil --set HostName "${HOST_NAME}" &&
+    sudo scutil --set LocalHostName "${HOST_NAME}"
 # Sets the system's network names for various protocols to $HOST_NAME.
 
 sudo systemsetup -setrestartfreeze on
@@ -48,15 +48,15 @@ sudo systemsetup -setrestartfreeze on
 
 # Customize System UI Server Preferences
 for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
-            sudo defaults write "${domain}" dontAutoLoad -array \
-                '/System/Library/CoreServices/Menu Extras/TimeMachine.menu' \
-                '/System/Library/CoreServices/Menu Extras/Volume.menu'
-		done \
-            && sudo defaults write com.apple.systemuiserver menuExtras -array \
-                '/System/Library/CoreServices/Menu Extras/Bluetooth.menu' \
-                '/System/Library/CoreServices/Menu Extras/AirPort.menu' \
-                '/System/Library/CoreServices/Menu Extras/Battery.menu' \
-                '/System/Library/CoreServices/Menu Extras/Clock.menu'
+    sudo defaults write "${domain}" dontAutoLoad -array \
+        '/System/Library/CoreServices/Menu Extras/TimeMachine.menu' \
+        '/System/Library/CoreServices/Menu Extras/Volume.menu'
+done &&
+    sudo defaults write com.apple.systemuiserver menuExtras -array \
+        '/System/Library/CoreServices/Menu Extras/Bluetooth.menu' \
+        '/System/Library/CoreServices/Menu Extras/AirPort.menu' \
+        '/System/Library/CoreServices/Menu Extras/Battery.menu' \
+        '/System/Library/CoreServices/Menu Extras/Clock.menu'
 # Customizes the items in the menu bar. It removes TimeMachine and Volume menus and adds Bluetooth, Airport (Wi-Fi), Battery, and Clock menus. This loop goes through all the domain files for the system UI server in the user's preferences.
 
-killall "SystemUIServer" &> /dev/null
+killall "SystemUIServer" &>/dev/null
